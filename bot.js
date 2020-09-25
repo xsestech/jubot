7c217167a66c9e4df9bebfbcc97bfa0aed561d5e
xsestech <xsestech@gmail.com>
2020-09-25 20:15:54 +0300

    finished project

--- bot.js
+++ bot.js
@@ -2,15 +2,23 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const config = require("./botcfg.json");
 const utf8 = require('utf8');
+const botdata = require("./botdata.json");
 
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 bot.commands = new Discord.Collection();
 
 const token = config.token;
 const prefix = config.prefix;
+const rolename = config.rolename;
+const emojinames = config.emojinames;
+const emojiroles = config.emojiroles;
+let bot_db = botdata;
 
-let rolename = ["683721522392727656","683721867252858905","683722236720709748","683723979273273345","683723708178759747","670164002827141145","670164093415456789","670164143466217482","670164229726273576","670163488613597189","670163129090572319","670163263564283904"];
-let emojinames = ["🌐","🖥️","📱","💾","🎮","📜","🔷","🕹️","☕","🟨","🐘","🐍"];
+function db_commit(){
+  fs.writeFile( "botdata.json", JSON.stringify(bot_db), "utf8", (err) => {
+    if(err) console.log(err);
+} );
+}
 
 fs.readdir('./cmds/',(err,files)=>{
     if(err) console.log(err);
@@ -26,10 +34,9 @@ fs.readdir('./cmds/',(err,files)=>{
 
 bot.on('ready', () => {
   console.log(`Бот запустился ${bot.user.username}`);
-//   bot.generateInvite(["ADMINISTRATOR"]).then(link =>{
-//       console.log(link)
-//   })
-    
+  bot.generateInvite(["ADMINISTRATOR"]).then(link =>{
+      console.log(link)
+  });
 });
 
 bot.on('message', async message => {
@@ -46,16 +53,21 @@ bot.on('message', async message => {
 });
 
 bot.on('messageReactionAdd', async (reaction,user)=>{
-    let counter = 1;
     for(i in rolename)
     {
       if(reaction.message.guild.member(user).roles.cache.has(rolename[i]))
       {
-        counter++;
+        if(bot_db["users"][reaction.message.guild.member(user).id.toString()] == null)
+        {
+          bot_db["users"][reaction.message.guild.member(user).id.toString()] = 1
+        } else {
+          bot_db["users"][reaction.message.guild.member(user).id.toString()] = parseInt(bot_db["users"][reaction.message.guild.member(user).id.toString()]) + 1
+        }
+        db_commit();
       }
     }
     console.log(counter)
-    if(counter>5&&!reaction.message.guild.member(user).hasPermission("ADMINISTRATOR")) return;
+    if(parseInt(bot_db["users"][reaction.message.guild.member(user).id.toString()])>5&&!reaction.message.guild.member(user).hasPermission("ADMINISTRATOR")) return;
     if(!user) return;
     if(user.bot) return;
     if(!reaction.message.channel.guild) return;
@@ -74,10 +86,8 @@ bot.on('messageReactionAdd', async (reaction,user)=>{
     // console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
     // The reaction is now also fully available and the properties will be reflected accurately:
     console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-    
+
     console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-    
-    
     for(let i in emojinames)
     {
 
@@ -88,14 +98,14 @@ bot.on('messageReactionAdd', async (reaction,user)=>{
             reaction.message.guild.member(user).roles.add(rolename[i]);
         }
     }
-  
+
 });
 bot.on('messageReactionRemove', async (reaction,user)=>{
   if(!user) return;
   if(user.bot) return;
   if(!reaction.message.channel.guild) return;
   if(!((reaction.message.id == config.msgspec) ||(reaction.message.id == config.msgspec2))) return;
-  
+
   // if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER)
   if (reaction.partial) {
     // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
@@ -110,10 +120,10 @@ bot.on('messageReactionRemove', async (reaction,user)=>{
   // console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
   // The reaction is now also fully available and the properties will be reflected accurately:
   console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-  
+
   console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-  
-  
+
+
   for(let i in emojinames)
   {
 
@@ -122,7 +132,9 @@ bot.on('messageReactionRemove', async (reaction,user)=>{
           let role = reaction.message.guild.roles.cache.get(rolename[i]);
           console.log(`removed role ${role.name}`);
           reaction.message.guild.member(user).roles.remove(rolename[i]);
-      }
+          bot_db["users"][reaction.message.guild.member(user).id.toString()] = parseInt(bot_db["users"][reaction.message.guild.member(user).id.toString()]) - 1
+          db_commit();
+        }
   }
 
 });
